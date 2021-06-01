6661b0462a619b2a6b5f95447584dbe6eb3b5a15
aguedafrazao <aguedaaraujo13@gmail.com>
2021-06-01 13:25:44 -0300

    localstorage

---
+++ components/createTask.js
@@ -0,0 +1,43 @@
+import ButtonDone from './taskDone.js'
+import ButtonDelete from './taskDelete.js'
+
+
+export const handleNewItem = (evento) => {
+    evento.preventDefault()
+    // inicializando a lista vazia, o parde vai transformar a string em objeto e pegar o item
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+    const list = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const calendar = document.querySelector('[data-form-date]')
+    // moment é uma library do js para pegar a data
+    const date = moment(calendar.value)
+
+    const dateFormat = date.format('DD/MM/YYYY')
+    const data = {
+        valor, dateFormat
+    }
+    // aqui eu criei uma lista e nela eu passo os valores a serem preenchidos.  '...' pega tudo que tem dentro da tarefa
+    const updateTasks = [...tasks, data]
+    const createTask = NewTask(data)
+    // armazenar os dados temporariamente e transforma o objeto em string
+    localStorage.setItem("tasks", JSON.stringify(updateTasks))
+    list.appendChild(createTask)
+    input.value = " "
+
+}
+
+
+export const NewTask = ({ valor, dateFormat }) => {
+    const task = document.createElement('li')
+    task.classList.add('task')
+    const conteudo = `<p class="content">${dateFormat} * ${valor} </p>`
+
+    task.innerHTML = conteudo
+
+    task.appendChild(ButtonDone())
+    task.appendChild(ButtonDelete())
+    return task
+}
--- main.js
+++ main.js
@@ -1,37 +1,4 @@
-import ButtonDone from './components/taskDone.js'
-import ButtonDelete from './components/taskDelete.js'
-
-const handleNewItem = (evento) => {
-    evento.preventDefault()
-    const list = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
-
-    const calendar = document.querySelector('[data-form-date]')
-    const date = moment(calendar.value)
-
-    const dateFormat = date.format('DD/MM/YYYY')
-    const data = {
-        valor, dateFormat
-    }
-    const createTask = newTask(data)
-    list.appendChild(createTask)
-    input.value = " "
-
-}
-
-
-const newTask = ({ valor, dateFormat }) => {
-    const task = document.createElement('li')
-    task.classList.add('task')
-    const conteudo = `<p class="content">${dateFormat} * ${valor} </p>`
-
-    task.innerHTML = conteudo
-
-    task.appendChild(ButtonDone())
-    task.appendChild(ButtonDelete())
-    return task
-}
+import {handleNewItem} from './components/createTask.js'
 
 const addTask = document.querySelector('[data-form-button]')
 addTask.addEventListener('click', handleNewItem)
