35a3b9c3240f66e87df79aa5ac3e07cf56ea4d16
aguedafrazao <aguedaaraujo13@gmail.com>
2021-06-01 11:44:11 -0300

    added datetime

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 import ButtonDone from './components/taskDone.js'
 import ButtonDelete from './components/taskDelete.js'
 
-    const newTask = (evento) => {
+const handleNewItem = (evento) => {
     evento.preventDefault()
     const list = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
+
+    const calendar = document.querySelector('[data-form-date]')
+    const date = moment(calendar.value)
+
+    const dateFormat = date.format('DD/MM/YYYY')
+    const data = {
+        valor, dateFormat
+    }
+    const createTask = newTask(data)
+    list.appendChild(createTask)
+    input.value = " "
+
+}
+
+
+const newTask = ({ valor, dateFormat }) => {
     const task = document.createElement('li')
     task.classList.add('task')
-    
-    const conteudo = `<p class="content">${valor}</p>`
-    
+    const conteudo = `<p class="content">${dateFormat} * ${valor} </p>`
+
     task.innerHTML = conteudo
 
     task.appendChild(ButtonDone())
     task.appendChild(ButtonDelete())
-    list.appendChild(task)
-    input.value = " "
+    return task
 }
 
 const addTask = document.querySelector('[data-form-button]')
-addTask.addEventListener('click', newTask)
+addTask.addEventListener('click', handleNewItem)
